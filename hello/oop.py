3d077c77f87ee693a7987058955d08dc2ef9afe2
supremeyh <supremeyh@126.com>
2018-10-31 09:52:18 +0800

    10.31 类属性和实例属性

--- hello/oop.py
+++ hello/oop.py
@@ -73,3 +73,11 @@ dir('ABC')
 # getattr()、setattr() 、hasattr() ， 直接操作一个对象的状态
 hasattr(Student, 'y') # 有属性'y'吗？ # False
 
+
+# 实例属性和类属性
+# 实例属性属于各个实例所有，互不干扰；类属性属于类所有，所有实例共享一个属性；不要对实例属性和类属性使用相同的名字，否则将产生难以发现的错误。
+class School(object):
+    name = 'Peking University' # 类属性,类本身需要绑定一个属性
+    def __init__(self, location):  # 给实例绑定属性的方法是通过实例变量，或者通过self变量
+        self.location = location
+    
