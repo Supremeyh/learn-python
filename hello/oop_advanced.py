d8f56f76773cf5aab41f8a06b1f8ed2fefbc2992
supremeyh <supremeyh@126.com>
2018-11-02 13:07:29 +0800

    11.2 枚举类 Enum

--- hello/oop_advanced.py
+++ hello/oop_advanced.py
@@ -168,4 +168,34 @@ b()  # self参数不要传入. the book name is Game of Thrones.
 callable(Book()) # True
 
 
+# 枚举类
+# Enum可以把一组相关常量定义在一个class中，且class不可变，而且成员可以直接比较。
+from enum import Enum
+
+Month = Enum('Month',  ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+# 这样我们就获得了Month类型的枚举类，可以直接使用Month.Jan来引用一个常量，或者枚举它的所有成员：
+# 既可以用成员名称引用枚举常量，又可以直接根据value的值获得枚举常量。
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+# Jan => Month.Jan , 1    Feb => Month.Feb , 2    ......    Dec => Month.Dec , 12
+
+
+# 如果需要更精确地控制枚举类型，可以从Enum派生出自定义类：
+from enum import Enum, unique
+@unique   # @unique装饰器可以帮助我们检查保证没有重复值。
+class Weekday(Enum):
+    Sun = 0 # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+
+
+
+
+
+
 
