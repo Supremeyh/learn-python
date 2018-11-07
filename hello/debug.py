8d7ffe4f46531588993d47fea3e43eb9e20bf36f
supremeyh <supremeyh@126.com>
2018-11-07 22:10:26 +0800

    11.7 单元测试

--- hello/debug.py
+++ hello/debug.py
@@ -62,6 +62,86 @@ print(10 / n)
 
 
 
+# 单元测试, 用来对一个模块、一个函数或者一个类来进行正确性检验的测试工作。
+# “测试驱动开发”（TDD：Test-Driven Development）
+# 以测试为驱动的开发模式最大的好处就是确保一个程序模块的行为符合我们设计的测试用例。在将来修改的时候，可以极大程度地保证该模块行为仍然是正确的。
+# 单元测试可以有效地测试某个程序模块的行为，是未来重构代码的信心保证。单元测试的测试用例要覆盖常用的输入组合、边界条件和异常。单元测试代码要非常简单。
+# 如 mydict.py
+class Dict(dict):
+  def __init__(self,**kw):
+    super().__init__(**kw)
+  def __getattr__(self,key):
+    try:
+      return self[key]
+    except KeyError:
+      raise AttributeError(r "'Dic' object has no attribute '%s'" % key)
+  def __setattr__(self, key, value):
+    return self[key] = value
+  
+# 为了编写单元测试，我们需要引入Python自带的unittest模块
+# mydict_test.py 测试代码
+import unittest
+
+from mydict import Dict
+
+class TeatDict(unittest.TestCase): # 编写单元测试, 写测试类，从unittest.TestCase继承
+  def test_init(self):  # 以test开头的方法就是测试方法，不以test开头的方法不被认为是测试方法，测试的时候不会被执行。
+    d = Dict( a=1, b='test')
+    self.assertEqual(d.a, 1)
+    self.assertEqual(d.b, 'test')
+    self.assertTrue(isinstance(d, dict))
+
+  def test_key(self):
+    d = Dict()
+    d['key'] = 'value'
+    self.assertEqual(d.key, 'value')
+  
+  def test_attr(self):
+    d = Dict()
+    d.key = 'value'
+    self.assertTrue('key' in d)
+    self.assertEqual(d['key'], 'value')
+  
+  def test_keyerror(self):
+    d = Dict()
+    with self.assertRaises(KeyError):
+      value = d['empty']
+  
+  def test_attrerror(self):
+    d = Dict()
+    with self.assertRaises(AttributeError):
+      value = d.empty()
+
+  def setUp(self):
+    print('setUp...')
+  
+  def tearDown(self):
+    print('tearDown...')
+  
+
+
+# 运行单元测试:
+# 测试代码末尾加上
+if __name__ == '__main':
+  unittest.main()
+# 这样就可以把mydict_test.py当做正常的python脚本运行
+
+# 在命令行通过参数-m unittest直接运行单元测试：
+python -m unittest mydict_test.py
+# 这样可以一次批量运行很多单元测试，并且，有很多工具可以自动来运行这些单元测试
+
+
+# setUp与tearDown
+# 可以在单元测试中编写两个特殊的setUp()和tearDown()方法。这两个方法会分别在每调用一个测试方法的前后分别被执行。
+
+
+
+
+
+
+
+
+
 
 
 
