6190eba66a31f632d76b3dbef26d7779221a46b9
supremeyh <supremeyh@126.com>
2018-11-21 23:23:23 +0800

    11.21 async/await

--- hello/asyncio.py
+++ hello/asyncio.py
@@ -100,6 +100,14 @@ produce(c)
 
 
 
+# async/await
+# 为了简化并更好地标识异步IO，从Python 3.5开始引入了新的语法async和await，可以让coroutine的代码更简洁易读。
+# async和await是针对coroutine的新语法，要使用新的语法，只需要做两步简单的替换： 把@asyncio.coroutine替换为async；把yield from替换为await
+
+
+
+
+
 
 
 
