77e46261637ec25d603e642a80d20442adfb2d11
supremeyh <supremeyh@126.com>
2018-11-23 22:14:12 +0800

    async/await

--- hello/asyncio.py
+++ hello/asyncio.py
@@ -102,12 +102,22 @@ produce(c)
 
 # async/await
 # 为了简化并更好地标识异步IO，从Python 3.5开始引入了新的语法async和await，可以让coroutine的代码更简洁易读。
-# async和await是针对coroutine的新语法，要使用新的语法，只需要做两步简单的替换： 把@asyncio.coroutine替换为async；把yield from替换为await
+# async和await是针对coroutine的新语法，要使用新的语法，只需要做两步简单的替换： 把@asyncio.coroutine替换为async；把yield from替换为await，其余的代码保持不变。
+import asyncio
+
 @asyncio.coroutine
 def hello():
   print('hello python!')
-  r = yield from asyncio.sleep(1)
+  r = yield from asyncio.sleep(1)  # 异步调用asyncio.sleep(1):
   print('see u again')
+
+# 获取EventLoop:
+loop = asyncio.get_event_loop()
+# 执行coroutine
+loop.run_until_complete(hello())
+loop.close(
+  
+)
 
 # 重新编写
 async def hello():
